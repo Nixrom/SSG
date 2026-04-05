7354946a4ff07dd9926391b9eb2070f24b40c650
Nixrom <spm03green@gmail.com>
2026-04-05 00:42:02 -0400

    12: main.py copy overwrite function.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
 from textnode import TextNode
+import os
+import shutil
+
 def main():
-    out = TextNode("Anchor Text", "Bold", "https://www.Test.test")
-    print(out)
+    replace_with_copy("static", "public", True)
+
+def replace_with_copy(source, destination, delete=False):
+    src = os.path.abspath(source)
+    dst = os.path.abspath(destination)
+    if delete == True:
+        shutil.rmtree(dst)
+        os.mkdir(dst)
+    for entity in os.listdir(src):
+        ent = os.path.join(src, entity)
+        if os.path.isfile(ent):
+            shutil.copy(ent, dst)
+        if os.path.isdir(ent):
+            copy_ent = os.path.join(dst, entity)
+            os.mkdir(copy_ent)
+            replace_with_copy(ent, copy_ent)
 
 main()
