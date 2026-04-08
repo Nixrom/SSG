4d03c904ea5f203406c9be22a786ad25476d0586
Nixrom <spm03green@gmail.com>
2026-04-08 17:49:55 -0400

    15: Build function.

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ from node_change import markdown_to_html_node
 from node_change import extract_title
 import re
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print (f"Generating page from {from_path} to {dest_path} using {template_path}.")
     markdown_from = open(from_path).read()
     html_template = open(template_path).read()
@@ -11,18 +11,20 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_from)
     html_filled = html_template.replace("{{ Title }}", title)
     html_filled = html_filled.replace("{{ Content }}", html_from)
+    html_filled = html_filled.replace("href=\"/", f"href=\"{basepath}")
+    html_filled = html_filled.replace("src=\"/", f"src=\"{basepath}")
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
     if not os.path.exists(dest_path):
         open(dest_path, mode="x")
     open(dest_path, mode="w").write(html_filled)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for entity in os.listdir(dir_path_content):
         ent = os.path.join(dir_path_content, entity)
         dst_path = os.path.join(dest_dir_path, entity)
         if os.path.isfile(ent) and re.findall(r".+.md(?!.)", entity):
             dst_path = dst_path.replace(".md", ".html")
-            generate_page(ent, template_path, dst_path)
+            generate_page(ent, template_path, dst_path, basepath)
         if os.path.isdir(ent):
-            generate_pages_recursive(ent, template_path, dst_path)
+            generate_pages_recursive(ent, template_path, dst_path, basepath)
--- src/main.py
+++ src/main.py
@@ -2,16 +2,20 @@ from textnode import TextNode
 from copy_overwrite import replace_with_copy
 from generate_page import generate_pages_recursive
 import os
+import sys
 
 src = os.path.abspath("static")
 src_2 = os.path.abspath("content")
-dst = os.path.abspath("public")
+dst = os.path.abspath("docs")
 gn_tmpl = os.path.abspath("template.html")
+basepath = "/"
+if sys.argv[0] != "":
+    basepath = sys.argv[0]
 
 def main():
     replace_with_copy(src, dst, True)
     replace_with_copy(src_2, dst)
-    generate_pages_recursive(src_2, gn_tmpl, dst)
+    generate_pages_recursive(src_2, gn_tmpl, dst, basepath)
 
 
 main()
